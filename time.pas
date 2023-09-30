44fa19ceec47ec9d9cfc52f4f55df5dcfaf9117c
Писанко Александр <avpisanko@mail.ru>
2023-09-30 10:39:49 +0300

    Добавлены изменения в time и созадан файл timer

--- time.pas
+++ time.pas
@@ -1,7 +1,7 @@
 ﻿begin
   var yearnum := ReadInteger('Введите номер года: ');
   if ((yearnum mod 4 <> 0) or ((yearnum mod 100 = 0) and (yearnum mod 400 <> 0))) then
-    print('Високосный год: False')
+    print('Високосный год: 365')
   else
-    print('Високосный год: True')
+    print('Високосный год: 366')
 end.
---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var hours := ReadInteger('Введите количество часов: ');
+  var mins := ReadInteger('Введите количество минут: ');
+  var sec := ReadInteger('Введите количество секунд: ');
+  Print(hours * 3600 + mins * 60 + sec, 'секунд.');
+end.
