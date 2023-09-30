0562ae01f1cd5b10974dcf43fd780b6eb8fad0a9
Писанко Александр <avpisanko@mail.ru>
2023-09-30 10:23:19 +0300

    Приветствие, зависимое от времени

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Print('Hello world!');
+  var time := ReadInteger('Введите час от 0 до 23');
+  case time of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!');
+    else Print('Доброй ночи, мир!');
+  end;
 end.
