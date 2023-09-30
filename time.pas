e3881889978641b188a573e67fcbf732b5d9390e
Писанко Александр <avpisanko@mail.ru>
2023-09-30 10:35:00 +0300

    Программа отпределяет високосный год

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var yearnum := ReadInteger('Введите номер года: ');
+  if ((yearnum mod 4 <> 0) or ((yearnum mod 100 = 0) and (yearnum mod 400 <> 0))) then
+    print('Високосный год: False')
+  else
+    print('Високосный год: True')
+end.
