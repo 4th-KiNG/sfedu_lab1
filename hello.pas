f38a661f3bd398a8743856e6e6ca22792d2d7167
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2023-10-08 13:15:13 +0300

    Update hello.pas

--- hello.pas
+++ hello.pas
@@ -1,9 +1,31 @@
 ﻿begin
   var time := ReadInteger('Введите час от 0 до 23');
-  case time of
-    4..10: Print('Доброе утро, мир!');
-    11..16: Print('Добрый день, мир!');
-    17..22: Print('Добрый вечер, мир!');
-    else Print('Доброй ночи, мир!');
+  var lang := ReadInteger('Выберите язык: 1 - Русский, 2 - English, 3 - Français');
+  if lang = 1 then
+    begin
+      case time of
+      4..10: Print('Доброе утро, мир!');
+      11..16: Print('Добрый день, мир!');
+      17..22: Print('Добрый вечер, мир!');
+      else Print('Доброй ночи, мир!');
+    end;
+  end;
+  if lang = 2 then
+    begin
+      case time of
+      4..10: Print('Good morning, world!');
+      11..16: Print('Good afternoon, world!');
+      17..22: Print('Good evening, world!');
+      else Print('Good night, world!');
+    end;
+  end;
+  if lang = 3 then
+    begin
+      case time of
+      4..10: Print('Bonjour le monde!');
+      11..16: Print('Bonjour, monde!');
+      17..22: Print('Bonsoir, monde!');
+      else Print('Bonne nuit, le monde!');
+    end;
   end;
 end.
